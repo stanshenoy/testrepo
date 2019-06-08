e44556ba0105b83089531cd61d34b4018a0aae98
thecodenomad@gmail.com <thecodenomad@gmail.com>
2019-06-08 21:21:06 +0800

    Updated repo

--- test.java
+++
@@ -1,5 +0,0 @@
-public void runCustomTrigger(String script) {
-    ScriptEngineManager factory = new ScriptEngineManager();
-    ScriptEngine engine = factory.getEngineByName("JavaScript");
-    engine.eval(script);
-}
